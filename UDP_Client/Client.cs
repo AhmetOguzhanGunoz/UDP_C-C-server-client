cf757327dea5acc90606ba460fedb24b9b8c3b0c
AhmetOguzhanGunoz <oguzhangunoz@gmail.com>
2017-06-12 20:17:32 +0300

    Prototype of UDP Communication
    This console application consist of two classes which are client and server. These classes rely on UDP communication between them with a message. This app can be developed to a communication tool or can be referenced to built communication apps.

---
+++ UDP_Client/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDP_Client
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            UdpClient client = new UdpClient("127.0.0.1", 9050);
+            byte[] data = new byte[1024];
+
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            string welcome = "Hi, are there anybody?";
+            data = Encoding.ASCII.GetBytes(welcome);
+
+            server.SendTo(data, data.Length, SocketFlags.None, ipep);
+
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint Remote = (EndPoint)sender;
+
+            data = new byte[1024];
+            int recv = server.ReceiveFrom(data,ref Remote);
+
+            Console.WriteLine("Message from {0}: ", Remote.ToString());
+            Console.WriteLine(Encoding.ASCII.GetString(data,0,recv));
+
+            Console.ReadLine();
+            server.Close();
+        }
+    }
+}
---
+++ UDP_Server/Server.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDP_Server
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            int recv;
+            byte[] data = new byte[1024];
+
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+            Socket newsock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            newsock.Bind(ipep);
+
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint Remote = (EndPoint)sender;
+
+            recv = newsock.ReceiveFrom(data, ref Remote);
+            Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+
+            string welcome = "welcome to test server";
+            data = Encoding.ASCII.GetBytes(welcome);
+            newsock.SendTo(data, data.Length, SocketFlags.None, Remote);
+
+            Console.ReadLine();
+        }
+    }
+}
